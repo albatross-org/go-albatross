4e335d8e5f4e9e3bf8997e610a205dc3cc1ca39b
Olly Britton <ollybritton@gmail.com>
2021-01-02 18:10:02 +0000

    improve graph output

--- cmd/albatross/cmd/action_export_graph.go
+++ cmd/albatross/cmd/action_export_graph.go
@@ -3,13 +3,8 @@ package cmd
 import (
 	"bytes"
 	"fmt"
-	"os"
-	"strings"
-	"text/template"
+	"strconv"
 
-	"github.com/albatross-org/go-albatross/entries"
-
-	"github.com/Masterminds/sprig"
 	"github.com/spf13/cobra"
 )
 
@@ -17,34 +12,59 @@ import (
 var ActionExportGraphCmd = &cobra.Command{
 	Use:     "graph",
 	Aliases: []string{"dot", "graphviz"},
-	Short:   "print a DOT-formatted graph representing the entries",
+	Short: `print a DOT-formatted graph representing the entries
+	
+This command can be used to visualise a store using Graphviz software by outputting a DOT formatted representation
+of all entries and their links.
+
+For example, to export all entries:
+
+	$ albatross get export graph | dot -T svg -o /tmp/out.svg
+	digraph {
+		"notes/books/flow" -> "notes/books/flow/chapters/1-happiness-revisited"
+		...
+	}
+	
+You can specify graph attributes using the --graph-attributes/-g flag:
+
+	$ albatross get export graph -g overlap=scale -g fontname=Times-Roman | dot -T svg -o /tmp/out.svg
+	# digraph {
+		overlap=scale
+		fontname=Times-Roman
+		"notes/books/flow" -> "notes/books/flow/chapters/1-happiness-revisited"
+		...	
+	}
+	
+Currently, only displaying entry paths is supported. Entries that link to other entries outside the scope
+of the search will not be displayed.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		_, collection, list := getFromCommand(cmd)
 
-		var tmpl = template.New("input").Funcs(sprig.TxtFuncMap())
-
-		tmplStr := "{{.Path}}"
-		tmpl, err := tmpl.Parse(tmplStr)
-		if err != nil {
-			fmt.Println("Error parsing template:")
-			fmt.Println(err)
-			os.Exit(1)
-		}
+		attributes, err := cmd.Flags().GetStringToString("graph-attributes")
+		checkArg(err)
 
 		var out bytes.Buffer
+
 		out.WriteString("digraph {\n")
+		for key, val := range attributes {
+			out.WriteString("\t")
+			out.WriteString(key)
+			out.WriteString("=")
+			out.WriteString(val)
+			out.WriteString("\n")
+		}
 
 		for _, entry := range list.Slice() {
-			// TODO: this is a rough escape
-			stringerEntry := entries.NewStringerTemplate(entry, tmpl)
-			escapedOutput := strings.ReplaceAll(stringerEntry.String(), `"`, `\"`)
+			escapedOutput := strconv.Quote(entry.Path)
 
 			for _, link := range entry.OutboundLinks {
 				linkedEntry := collection.ResolveLink(link)
-				stringerLink := entries.NewStringerTemplate(linkedEntry, tmpl)
-				escapedLink := strings.ReplaceAll(stringerLink.String(), `"`, `\"`)
+				if linkedEntry == nil {
+					continue
+				}
+				escapedLink := strconv.Quote(linkedEntry.Path)
 
-				out.WriteString(fmt.Sprintf("\t\"%s\" -> \"%s\"\n", escapedOutput, escapedLink))
+				out.WriteString(fmt.Sprintf("\t%s -> %s\n", escapedOutput, escapedLink))
 			}
 		}
 
@@ -56,4 +76,5 @@ var ActionExportGraphCmd = &cobra.Command{
 
 func init() {
 	ActionExportCmd.AddCommand(ActionExportGraphCmd)
+	ActionExportGraphCmd.Flags().StringToStringP("graph-attributes", "g", map[string]string{}, "graph attributes (https://graphviz.org/doc/info/attrs.html)")
 }
